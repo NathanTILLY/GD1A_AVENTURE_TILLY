73314c2213864c4410c0658d776c71bdf529c2fa
NathanTILLY <74654828+NathanTILLY@users.noreply.github.com>
2021-05-05 13:04:52 +0200

    update

--- src/main.js
+++ src/main.js
@@ -2,20 +2,25 @@ import Phaser from './lib/phaser.js'
 
 import Game from './scenes/Game.js'
 import GameOver from './scenes/GameOver.js'
+import Lore from './scenes/Lore.js'
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
-    width: 1024,
-    height: 576,
-    scene: [Game, GameOver],
+    width: 1280,
+    height: 720,
+    scene: [Lore, Game, GameOver],
     physics: {
         default: 'arcade',
         arcade: {
             gravity: {
-                y: 200
-            },
-            debug: false
+                y: 0
+            }
+          
         }
     },
     input:{gamepad:true},
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
 })
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,11 +1,15 @@
-import Phaser from './lib/phaser.js'
+import Phaser from '../lib/phaser.js'
 
 export default class Game extends Phaser.Scene
 {
     constructor()
     {
-        super(game);
+        super('game');
     }
+    init(data){
+		this.attack = data.attack
+		this.life = data.health
+	}
 
     //---------------------------------------------------------------------- VARIABLES ----------------------------------------------------------------------
     
@@ -20,10 +24,10 @@ export default class Game extends Phaser.Scene
 
         this.load.tilemapTiledJSON('Map', 'ZeldaLike.json');
 
-        this.load.image('pdv1', 'assets/pointDeVie1.png');
-        this.load.image('pdv2', 'assets/pointDeVie2.png');
-        this.load.image('pdv3', 'assets/pointDeVie3.png');
-        this.load.spritesheet('ours', 'assets/ours.png', { frameWidth: 64, frameHeight: 94 });
+        this.load.image('Pdv', 'assets/Pdv.png');
+        this.load.image('PdvPerdu', 'assets/PdvPerdu.png');
+      
+        this.load.spritesheet('ours', 'assets/chevalier.png', { frameWidth: 24, frameHeight: 37 });
         this.load.spritesheet('ennemi', 'assets/ennemi.png', { frameWidth: 64, frameHeight: 94 });
     }
 
@@ -31,98 +35,186 @@ export default class Game extends Phaser.Scene
 
     create ()
     {
+        this.life = 3
+        //Vie
+        if (this.life == 3){
+			this.pdv1 = this.add.image(40,50,'Pdv').setScale(2).setScrollFactor(0).setDepth(3);
+			this.pdv2 = this.add.image(85,50,'Pdv').setScale(2).setScrollFactor(0).setDepth(3);
+			this.pdv3 = this.add.image(130,50,'Pdv').setScale(2).setScrollFactor(0).setDepth(3);
+		}
+
+		else if (this.life == 2){
+			this.pdv1 = this.add.image(40,50,'Pdv').setScale(2).setScrollFactor(0).setDepth(3);
+			this.pdv2 = this.add.image(92,50,'Pdv').setScale(2).setScrollFactor(0).setDepth(3);
+            this.pdvDead = this.add.image(145,45,'PdvPerdu').setScale(2).setScrollFactor(0).setDepth(3);
+		}
+		else if (this.life == 1){
+			this.pdv1 = this.add.image(40,50,'Pdv').setScale(2).setScrollFactor(0).setDepth(3);
+            this.pdvDead = this.add.image(92,45,'PdvPerdu').setScale(2).setScrollFactor(0).setDepth(3);
+            this.pdvDead = this.add.image(165,45,'PdvPerdu').setScale(2).setScrollFactor(0).setDepth(3);
+		}
+
         let Village = this.make.tilemap({key:'Map'});
 
         let Terrain = Village.addTilesetImage('Serene_Village_32x32','Tileset');
 
         let Background = Village.createLayer('Background', Terrain, 0, 0).setDepth(-2);
         let Layer1 = Village.createLayer('ElemDecor', Terrain, 0, 0).setDepth(-1);
-    cursors = this.input.keyboard.createCursorKeys();
+        this.cursors = this.input.keyboard.createCursorKeys();
 
         
         
         //---------------------------------------------------------------------- PLAYER ----------------------------------------------------------------------
             
-            player = this.physics.add.sprite(100, 1910, 'ours');
-            player.direction = 'right';
-            player.setBounce(0);
-            player.setCollideWorldBounds(true);
-            
-            hp = this.add.image(175,900, "pdv3").setScrollFactor(0);
+            this.player = this.physics.add.sprite(200, 1900, 'ours');
+            this.player.direction = 'right';
+            this.player.setBounce(0);
+            this.player.setCollideWorldBounds(true);
 
-            this.physics.add.collider(player, Background);
+            this.physics.add.collider(this.player, Background);
 
             Background.setCollisionByProperty({collides:true});
         //---------------------------------------------------------------------- ANIMS ----------------------------------------------------------------------   
             
             this.anims.create({
                 key: 'left',
-                frames: this.anims.generateFrameNumbers('ours', { start: 0, end: 3 }),
+                frames: this.anims.generateFrameNumbers('ours', { start: 0, end: 2 }),
                 frameRate: 10,
-                repeat: 1
+                repeat: -1
             });
 
             this.anims.create({
                 key: 'turn',
-                frames: [ { key: 'ours', frame: 4 } ],
+                frames: [ { key: 'ours', frame: 7 } ],
                 frameRate: 20
             });
 
             this.anims.create({
                 key: 'right',
-                frames: this.anims.generateFrameNumbers('ours', { start: 5, end: 8 }),
-                frameRate: 11,
-                repeat: 1
+                frames: this.anims.generateFrameNumbers('ours', { start: 9, end: 11 }),
+                frameRate: 10,
+                repeat: -1
             });
 
+            this.anims.create({
+                key: 'up',
+                frames: this.anims.generateFrameNumbers('ours', { start: 3, end: 5 }),
+                frameRate: 10,
+                repeat: -1
+            });
+            this.anims.create({
+                key: 'down',
+                frames: this.anims.generateFrameNumbers('ours', { start: 6, end: 8 }),
+                frameRate: 10,
+                repeat: -1
+            });
+        
+            this.physics.add.collider(this.player, Background);
+        
+            Background.setCollisionByProperty({collide:true});
+    
+            this.physics.add.collider(this.player, Layer1);
+            
+            Layer1.setCollisionByProperty({collide:true});
+    
+
+    
+    
+             const debugGraphics = this.add.graphics().setAlpha(0.75);
+            Background.renderDebug(debugGraphics, {
+                  tileColor: null, // Color of non-colliding tiles
+                  collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+                  faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+            });
+            Layer1.renderDebug(debugGraphics, {
+                tileColor: null, // Color of non-colliding tiles
+                collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+                faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+              });
+            
+        
         //camera
-        this.cameras.main.startFollow(player);
+        this.cameras.main.startFollow(this.player);
         this.cameras.main.setBounds(0,0,Village.widthInPixels, Village.heightInPixels);
         this.physics.world.setBounds(0,0, Village.widthInPixels, Village.heightInPixels);
-        player.setCollideWorldBounds(true);
+        this.player.setCollideWorldBounds(true);
 
     }
     //----------------------- UPDATE -------------------------------------------------------------------------------------------------------------------------
-    update ()
+    update (t,dt)
     {
-        player.setVelocity(0);
-        if (cursors.left.isDown)  {
-                player.direction = 'left';
-                if (player.body.touching.down){
-                    player.setVelocityX(-400);
-                }
-                else if (player.body.touching.grandePlateformeGlace){
-                    player.setVelocityX(-4000);
-                }
-                else {
-                    player.setVelocityX(-600);
-                }
-                player.anims.play('left', true);
-            }
-            else if (cursors.right.isDown) {
-                player.direction = 'right';
-                if (player.body.touching.down){
-                    player.setVelocityX(400); 
-                }
-                else if (player.body.touching.grandePlateformeGlace){
-                    player.setVelocityX(4000);
-                }
-                else {
-                    player.setVelocityX(600);
-                }
-                player.anims.play('right', true);
-            }
-            else  {
-                player.setVelocityX(0);
-                player.anims.play('turn');
-            }
-            if (cursors.up.isDown ) {
-                player.setVelocityY(-950);
-            }
-            if (cursors.down.isDown )
-            {
-                player.setVelocityY(900);
-            }
-            
+        const speed = 275;
+
+        if (!this.player)
+		{
+			return
+		}
+
+        this.player.setVelocity(0)
+		this.player.anims.play('turn')
+
+        if (this.paddleConnected == true)
+    	{
+
+        	if (paddle.right)
+        	{
+            	this.player.setVelocityX(speed);
+            	//this.player.anims.play('right', true);
+        	}
+        	else if (paddle.left)
+        	{
+            	this.player.setVelocityX(-speed);
+            	//this.player.anims.play('left', true);
+        	}
+            else if (paddle.up)
+        	{
+            	this.player.setVelocityY(-speed);
+            	//this.player.anims.play('up', true);
+        	}
+            else if (paddle.down)
+        	{
+            	this.player.setVelocityY(speed);
+            	//this.player.anims.play('down', true);
+        	}
+
+			if (this.attack && paddle.A){
+				this.attaquer(this.player);
+			}
+
+		}
+
+		else if (this.cursors.up.isDown)
+		{
+			this.player.direction='up';
+			this.player.setVelocityY(-speed)
+			//this.player.anims.play('up', true);
+		}
+
+
+		else if (this.cursors.left.isDown)
+		{
+			this.player.direction='left';
+            this.player.setVelocityX(-speed)
+			//this.player.anims.play('left', true)
+		}
+
+		else if (this.cursors.right.isDown)
+		{
+			this.player.direction='right';
+            this.player.setVelocityX(speed)
+			//this.player.anims.play('right', true)	
+		}
+        else if (this.cursors.down.isDown)
+		{
+			this.player.direction='down';
+            this.player.setVelocityY(speed)
+			//this.player.anims.play('down', true)	
+		}	
+		
+		if (this.attack && Phaser.Input.Keyboard.JustDown(this.boutonAttaque)){
+			this.player.setVelocity(0)
+			this.attaquer(this.player);
+		}
+
     }
 }
