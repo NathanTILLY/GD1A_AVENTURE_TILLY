f2adbc8912608309b68aad34bbf5b64c62eb374d
NathanTILLY <74654828+NathanTILLY@users.noreply.github.com>
2021-05-08 05:28:04 +0200

    new map

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Phaser from './lib/phaser.js'
+import Dungeon from './scenes/Dungeon.js'
 
 import Game from './scenes/Game.js'
+import Dungeon from './scenes/Dungeon.js'
 import GameOver from './scenes/GameOver.js'
 import Lore from './scenes/Lore.js'
 
@@ -8,7 +10,7 @@ export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scene: [Lore, Game, GameOver],
+    scene: [Lore, Game, Dungeon, GameOver],
     physics: {
         default: 'arcade',
         arcade: {
